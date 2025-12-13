192ba1174d2180b8658af346fbc4c62d4136be2a
ram-kumar18 <onlystudy1804@gmail.com>
2025-12-13 21:22:56 +0530

    Add files via upload

---
+++ DAY06/PROJECT 6 - ESCAPING THE MAZE.py
@@ -0,0 +1,18 @@
+# GO TO URL : https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()       # NOTE : LOOK AFTER THIS PROJECT AFTER DAY 15 BECAUSE IT CONTAINS ONE INTERMEDIATARY STEP WHICH IS NOT TAUGHT TILL NOW
+                            # THERE'S ONE CONDITION WHEN THE FRONT IS ALSO CLEAR AND RIGHT IS ALSO CLEAR 
+                            # THAT'S WHY THE ROBOT STUCKS IN A LOOP AND MOVES IN A SQUARE PATTERN REPEATEDLY
