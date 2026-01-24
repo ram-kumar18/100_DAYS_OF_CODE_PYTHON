d9ce95579eaf692c3af74654f7c5238cf32617cb
ram-kumar18 <onlystudy1804@gmail.com>
2026-01-24 17:39:50 +0530

    Add files via upload

---
+++ DAY13/Describe_The_Problem.py
@@ -0,0 +1,17 @@
+# def my_function():
+#     for i in range(1, 20): 
+#         if i == 20:
+#             print("You got it")
+
+
+# my_function()
+
+def my_function():
+    for i in range(1, 21): # changed the upper limit to 21
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
+
